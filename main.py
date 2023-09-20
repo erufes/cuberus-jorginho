d781df77a077f990d9a8036653e62aa2d8a39943
Elisa Muller Sarmento <elisamullersarmento@pop-os.localdomain>
2023-09-20 17:17:04 -0300

    funções para verificar o movimento e esquemático das funções de movimentação

--- main.py
+++ main.py
@@ -1,52 +1,64 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from ev3dev2.sensor import Sensor, INPUT_1
-from ev3dev2.port import LegoPort
+#from pybricks.hubs import EV3Brick
+#from pybricks.ev3devices import Motor
+#from pybricks.parameters import Port, Direction
 
-from classes.braco  import Braco
-from classes.base   import Base
+#from classes.movimento import Movimento
+#from classes.braco  import Braco
+#from classes.base   import Base
+#from classes.sensor import Sensor
+import json
 
-from pybricks.iodevices import I2CDevice
-from pybricks.iodevices import  AnalogSensor
-from pybricks.iodevices import UARTDevice
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.media.ev3dev import Font
+arquivo = './../algoritmoDeSolucao/cubos/movimentos_cuboConfigEx1.json'
+# Create your objects here.
 
-import os
-import sys
-import time
+#ev3 = EV3Brick()
 
-from nxtcam.i2c import *
-from nxtcam.mindsensorsPYB import NXTCAM
+# Inicializa as partes do jorginho
 
-NXTCAM_ADDRESS = 0x02
-ev3 = EV3Brick()
-ev3.speaker.beep()
-# Set LEGO port for Pixy on input port 1
-#in1 = LegoPort(INPUT_1)
-#in1.mode = 'auto'
-# Wait 2 secs for the port to get ready
+#braco =  Braco(Motor(Port.A, Direction.CLOCKWISE, None))
+#base =  Base(Motor(Port.A, Direction.CLOCKWISE, None), ev3)
+#sensor =  Sensor(Motor(Port.A, Direction.CLOCKWISE, None))
+def verifica_sentido(movimento):
 
-time.sleep(2)
+        if movimento == 'Fi' or movimento == 'Ri' or movimento == 'Ui' or movimento == 'Li' or movimento == 'Bi' or movimento == 'Di' or movimento == 'Xi' or movimento == 'Yi' or movimento == 'Zi' or movimento == 'Mi'or movimento == 'Si' or movimento == 'Ei':
+            sentido = 'anti horario'
+        else:
+            sentido = 'horario'
+        return sentido
 
-# Connect Pixy camera
-#pixy = Sensor(INPUT_1)
+f = open(arquivo,"r")
+movimentos = json.loads(f.read())
 
-#initialize nxtcam
-cam = NXTCAM(Port.S1,NXTCAM_ADDRESS)
-
-cam.startTracking()
-cam.trackObject()
-
-b = cam.getBlobs(1)
-print("Color: " + str(b.color))
-print("Left: " + str(b.left))
-print("Top: " + str(b.top))
-print("Right: " + str(b.right))
-print("Bottom: " + str(b.bottom))
+for i in movimentos:
+    sentido = verifica_sentido(i)
+   #direcao, sentido = verifica_movimento(i)
 
 
+f.close()
 
+# # Movimento braço
+# # braco =  Braco(Motor(Port.A, Direction.CLOCKWISE, None))
+# # braco.set_sentido(1)
+#
+# # ev3.speaker.beep()
+#
+# # braco.set_movimenta()
 
+# # Movimento Base
+# # base =  Base(Motor(Port.A, Direction.CLOCKWISE, None), ev3)
+# # base.set_qtdMovimentos(4)
+# # base.set_Direcao('d')
+#
+# # base.set_movimenta()
+#     
+# # ev3.speaker.beep()
+
+# # Movimento sensor
+# # sensor =  Sensor(Motor(Port.A, Direction.CLOCKWISE, None))
+# # sensor.set_TimerLeituraCentro(2)
+# # sensor.set_TimerLeituraAresta(16)
+#
+# # sensor.set_movimenta()
+#
+# # ev3.speaker.beep()
