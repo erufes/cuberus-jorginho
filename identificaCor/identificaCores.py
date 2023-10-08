d430acabb6b888d2333c04accf8a73835c92447f
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2023-10-08 20:49:37 -0300

    :construction: cubo.txt

--- identificaCor/identificaCores.py
+++ identificaCor/identificaCores.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
-import os
 import json
+from rubik.cube import Cube
 
 boundaries = [                                                    #LISTA PARAMETRO DAS CORES
     ([50, 180, 210], [90, 210, 240]),      # amarelo 0
@@ -23,7 +23,28 @@ listaCoordenadas = [                                              #LISTA DAS COO
     [275, 182],         # coordenada 2,2      
 ]
 
-lista = ["Amarelo", "Verde", "Laranja", "Vermelho", "Branco", "Azul"]
+lista = [
+    ("Y", "Amarelo"),
+    ("G", "Verde"), 
+    ("O", "Laranja"), 
+    ("R", "Vermelho"), 
+    ("W", "Branco"), 
+    ("B", "Azul")  
+]
+
+def getSTRCubo(cubo, ordem_leitura):
+    ordem_str = [ordem_leitura[0], ordem_leitura[3], ordem_leitura[5],ordem_leitura[1],ordem_leitura[4],ordem_leitura[2]]
+    
+    # transforma em string face 1
+    str_concatenada = ''.join([elemento for linha in cubo[ordem_str[0]] for elemento in linha])
+    
+    # transforma em string faces de 2 a 5
+    str_concatenada += ''.join([elemento for i in range(3) for face in ordem_str[1:-1] for elemento in cubo[face][i]])
+    
+    # transforma em string face 6
+    str_concatenada += ''.join([elemento for linha in cubo[ordem_str[-1]] for elemento in linha])
+    
+    return str_concatenada
 
 def giraFace(face, sentido="antiorario", n=1):
     
@@ -60,14 +81,20 @@ def getCor(rgb):
             if ((lower[0] <= rgb[0]) and (rgb[0] <= upper[0])):
                 if ((lower[1] <= rgb[1]) and (rgb[1] <= upper[1])):
                     if ((lower[2] <= rgb[2]) and (rgb[2] <= upper[2])):
-                        return lista[parametroCor]
+                        return lista[parametroCor][0]
 
 def getCorFace(image):
     face, linha  = [], []
     
     for (y, x) in listaCoordenadas:
         rgb = image[y, x]
-        linha.append(getCor(rgb))
+        cor = getCor(rgb)
+        
+        if cor is not None: 
+            linha.append(cor)
+        else:
+            linha.append('N')
+            
         if len(linha) == 3:
             face.append(linha)
             linha = []
@@ -84,13 +111,16 @@ def getCubo():
         (h, w) = image.shape[:2]
         
         face = getCorFace(image)
-        cubo[face[1][1]] = face
-        ordem_leitura.append(face[1][1])
         
+        centro = list(filter(lambda x: x[0] == face[1][1], lista))[0][1]
+        
+        cubo[centro] = face
+        ordem_leitura.append(centro)
         # os.remove(arquivo)
-    return ordena(cubo, ordem_leitura)
+    
+    cubo = ordena(cubo, ordem_leitura)
+    return getSTRCubo(cubo, ordem_leitura)
 
 if __name__ == '__main__':
-    
-    with open('cubo.json', 'w') as json_file:
+    with open('cubo.txt', 'w') as json_file:
         json.dump(getCubo(), json_file, indent=2)
